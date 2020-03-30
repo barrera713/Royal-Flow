939d1f7aeb3b378d507d513e6b8c3d7fb50aad2a
Carlos Barrera <barrera9696@gmail.com>
2020-03-29 22:20:16 -0500

    something else

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ Item.belongsToMany(User, { through: Review, as: 'itemReviews', foreignKey: 'item
 
 // db.sync({ force: true });
 
+app.all('*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 
 //---------------------------------------Route Middleware --------------------------------------------
 app.use('/user', userRoute);
