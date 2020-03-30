2d9a2562c881f1fb70aef18d77cdaadae067587e
Carlos Barrera <barrera9696@gmail.com>
2020-03-29 23:16:29 -0500

    changed to *

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors({
     origin: [
-    'http://localhost:3001',
+    '*',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
